bc0e37aa23672770c261223c851f94b8558583f9
Shenzai <m.weiss27@yahoo.com>
2014-10-20 18:27:59 -0400

    Fixed NodeQuery

--- src/edu/osu/cse/misc/graph/pathfinding/wrappers/node/NodeQuery.java
+++ src/edu/osu/cse/misc/graph/pathfinding/wrappers/node/NodeQuery.java
@@ -1,9 +1,7 @@
 package edu.osu.cse.misc.graph.pathfinding.wrappers.node;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.TreeSet;
 
 public class NodeQuery extends LinkedList<Node> {
 
@@ -23,14 +21,5 @@ public class NodeQuery extends LinkedList<Node> {
 		}
 		return this;
 	}
-	
-	public NodeQuery sort(Comparator<Node> comparator) {
-		TreeSet<Node> sortedSet = new TreeSet<>(comparator);
-		sortedSet.addAll(this);
-		this.clear();
-		this.addAll(sortedSet);
-		return this;
-	};
-	
 	
 }
